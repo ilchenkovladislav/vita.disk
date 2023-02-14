8bd29e48b32ef56ff88fc76757d4f0b311028cc2
Ilchenko <ilchenkowd@gmail.com>
2023-02-14 11:54:05 +0500

    created folders for server and client and transferred files

---
+++ src/client/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+import { ProjectPage } from './pages/ProjectPage/ProjectPage';
+
+export default function App() {
+  return (
+    <div className="App">
+      <ProjectPage />
+    </div>
+  );
+}
