fdec9a3810cdedd4cad93371579072a77066869d
Ilchenko <ilchenkowd@gmail.com>
2023-03-06 16:51:16 +0500

    Added project initialization and admin panel output

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,11 +1,23 @@
+import { useEffect } from 'react';
+
+import { useStateSelector, useActionCreators } from './store/hooks';
+import { projectAsyncActions } from './store/slices/projectSlice';
+import { AdminPanel } from './pages/AdminPanel/AdminPanel';
 import './App.css';
 
-import { ProjectPage } from './pages/ProjectPage/ProjectPage';
-
 export default function App() {
+  const projectsStatus = useStateSelector((state) => state.project.status);
+  const actions = useActionCreators(projectAsyncActions);
+
+  useEffect(() => {
+    if (projectsStatus === 'init') {
+      actions.getProjects();
+    }
+  }, [projectsStatus]);
+
   return (
     <div className="App">
-      <ProjectPage />
+      <AdminPanel />
     </div>
   );
 }
