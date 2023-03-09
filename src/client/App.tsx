a1f917e22072a213c73a3f59f1a9339d11dcff7e
Ilchenko <ilchenkowd@gmail.com>
2023-03-09 09:57:12 +0500

    Added routes

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -7,6 +7,8 @@ import { ProjectPage } from './pages/ProjectPage/ProjectPage';
 import './App.css';
 
 import { Routes, Route } from 'react-router-dom';
+import { ProjectTab } from './components/ProjectTab/ProjectTab';
+import { ImageList } from './components/ImageList/ImageList';
 
 export default function App() {
   const projectsStatus = useStateSelector((state) => state.project.status);
@@ -22,7 +24,14 @@ export default function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<AdminPanel />} />
-        <Route path="/project/:projectId" element={<ProjectPage />} />
+        <Route path="/project/:projectId" element={<ProjectPage />}>
+          <Route path="" element={<ProjectTab />}>
+            <Route path="folder/:folderId" element={<ImageList />} />
+          </Route>
+          <Route path="settings" element={<h1>Настройки</h1>} />
+          <Route path="favourites" element={<h1>Избранное</h1>} />
+          <Route path="design" element={<h1>Дезигн</h1>} />
+        </Route>
       </Routes>
     </div>
   );
