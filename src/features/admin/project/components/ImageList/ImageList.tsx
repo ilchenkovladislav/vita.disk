9fe46ab5480167f2a7522eb541906049ea7062b9
Ilchenko <ilchenkowd@gmail.com>
2023-03-14 12:22:31 +0500

    Added image selection and deletion

--- src/features/admin/project/components/ImageList/ImageList.tsx
+++ src/features/admin/project/components/ImageList/ImageList.tsx
@@ -6,6 +6,7 @@ import { ImageUploader } from 'features/admin/project/components/ImageUploader/I
 
 import { useActionCreators } from 'store/hooks';
 import { imageAsyncActions } from 'store/slices/imageSlice';
+import { useEffect, useState } from 'react';
 
 export const ImageList = () => {
   const { projectId, folderId } = useParams();
@@ -17,37 +18,97 @@ export const ImageList = () => {
     )
   );
 
+  const [selectedImages, setSelectedImages] = useState<number[]>([]);
+
+  useEffect(() => {
+    setSelectedImages([]);
+  }, [folderId]);
+
+  function handleCheckboxClick(
+    e: React.FormEvent<HTMLInputElement>,
+    imageId: number
+  ) {
+    if (e.currentTarget.checked) {
+      setSelectedImages((prev) => [...prev, imageId]);
+    } else {
+      const idx = selectedImages.findIndex((id) => id === imageId);
+
+      setSelectedImages((prev) => [
+        ...prev.slice(0, idx),
+        ...prev.slice(idx + 1)
+      ]);
+    }
+  }
+
   function handleDeleteClick(id: number) {
     actions.deleteImage(id);
   }
 
+  function handleSelectAll() {
+    setSelectedImages(images.map((el) => el.id));
+  }
+
+  function handleDeselectAll() {
+    setSelectedImages([]);
+  }
+
+  function handleDeleteAll() {
+    for (const selectedImage of selectedImages) {
+      actions.deleteImage(selectedImage);
+    }
+
+    setSelectedImages([]);
+  }
+
   return (
-    <ul className={styles.images}>
-      <li>
-        {
-          <ImageUploader
-            projectId={Number(projectId)}
-            folderId={Number(folderId)}
-          />
-        }
-      </li>
-      {images.map((image) => (
-        <li className={styles.item} key={image.id}>
-          <img className={styles.img} src={image.path} alt="" />
-          <div className={styles.control}>
-            <button>переименовать</button>
-            <a href={image.path} download>
-              скачать
-            </a>
-            <button>скопировать ссылку</button>
-            <button onClick={() => handleDeleteClick(image.id)}>удалить</button>
-          </div>
-          <div className={styles.downloads}>
-            {image.numberDownloads} раз скачали
-          </div>
-          <input className={styles.check} type="checkbox" name="" id="" />
+    <>
+      <ul className={styles.images}>
+        <li>
+          {
+            <ImageUploader
+              projectId={Number(projectId)}
+              folderId={Number(folderId)}
+            />
+          }
         </li>
-      ))}
-    </ul>
+        {images.map((image) => (
+          <li
+            className={
+              selectedImages.includes(image.id)
+                ? styles.itemSelected
+                : styles.item
+            }
+            key={image.id}
+          >
+            <img className={styles.img} src={image.path} alt="" />
+            <div className={styles.control}>
+              <a href={image.path} download>
+                скачать
+              </a>
+              <button>скопировать ссылку</button>
+              <button onClick={() => handleDeleteClick(image.id)}>
+                удалить
+              </button>
+            </div>
+            <div className={styles.downloads}>
+              {image.numberDownloads} раз скачали
+            </div>
+            <input
+              className={styles.check}
+              onChange={(e) => handleCheckboxClick(e, image.id)}
+              type="checkbox"
+              checked={selectedImages.includes(image.id)}
+              name=""
+              id=""
+            />
+          </li>
+        ))}
+      </ul>
+      <div className={selectedImages.length ? '' : styles.pop}>
+        <button onClick={handleDeleteAll}>удалить выбранное</button>
+        <button onClick={handleSelectAll}>выделить всё</button>
+        <button onClick={handleDeselectAll}>снять выделение</button>
+      </div>
+    </>
   );
 };
