0664cc49b53b4fce72f5a73ce6a047949ff77aa2
Ilchenko <ilchenkowd@gmail.com>
2023-03-19 09:33:19 +0500

    added an increase in the image download counter

--- src/features/user/components/ImageItem/ImageItem.tsx
+++ src/features/user/components/ImageItem/ImageItem.tsx
@@ -2,6 +2,10 @@ import { FiDownload, FiShare2 } from 'react-icons/fi';
 import { AiOutlineHeart } from 'react-icons/ai';
 import { MdOutlineContentCopy } from 'react-icons/md';
 import { ImageItem as IImageItem } from 'store/slices/imageSlice';
+
+import { imageAsyncActions } from 'store/slices/imageSlice';
+import { useAppDispatch } from 'store/hooks';
+
 import styles from './ImageItem.module.scss';
 
 interface ImageItemProps {
@@ -13,6 +17,17 @@ export const ImageItem: React.FC<ImageItemProps> = ({
   image,
   handleClickLike
 }) => {
+  const dispatch = useAppDispatch();
+
+  function handleDownloadClick() {
+    dispatch(
+      imageAsyncActions.updateImage({
+        ...image,
+        numberDownloads: image.numberDownloads + 1
+      })
+    );
+  }
+
   return (
     <li className={styles.imageItem}>
       <img className={styles.image} src={image.path} alt="" />
@@ -28,7 +43,12 @@ export const ImageItem: React.FC<ImageItemProps> = ({
         >
           <AiOutlineHeart />
         </button>
-        <a className={styles.button} href={image.path} download>
+        <a
+          className={styles.button}
+          href={image.path}
+          download
+          onClick={handleDownloadClick}
+        >
           <FiDownload />
         </a>
       </div>
